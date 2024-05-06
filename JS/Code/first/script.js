365822b780f54191e8a873cd6d4f5c87bbb657db
prasad-bigdp <bigdp585@gmail.com>
2024-05-06 17:48:30 +0530

    update

---
+++ JS/Code/first/script.js
@@ -0,0 +1,56 @@
+// console.log("prasad")
+// alert("be careful")
+// //validation
+// console.log(confirm("Are you Sure?"))
+// console.log(prompt("enter a value"));
+
+var x= 30;
+console.log(x,typeof(x));
+x="raj"
+console.log(x, typeof x)
+x = true
+console.log(x, typeof x)
+x = 32.3698;
+console.log(x.toFixed(2))
+console.log(x.toPrecision(3))
+x = 23;
+console.log(x.toString(16))
+console.log(Math.abs(-16))
+console.log(Math.sqrt(16))
+console.log(Math.cbrt(27))
+console.log(Math.ceil(-4.1))
+console.log(Math.floor(3.3))
+console.log(Math.round(4.6))
+console.log(Math.pow(2,3), 2**3)
+console.log(Math.max(5, 6))
+//23.6985
+console.log(Math.min(5, 6))
+console.log(Math.round(Math.random() * 10000))
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
