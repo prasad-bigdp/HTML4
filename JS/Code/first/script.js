65fc47ab3a754aa2f7f4242dd1603af771e4ee37
prasad-bigdp <bigdp585@gmail.com>
2024-05-07 22:37:56 +0530

    update

--- JS/Code/first/script.js
+++ JS/Code/first/script.js
@@ -1,31 +1,31 @@
-// console.log("prasad")
-// alert("be careful")
-// //validation
-// console.log(confirm("Are you Sure?"))
-// console.log(prompt("enter a value"));
+// // // console.log("prasad")
+// // // alert("be careful")
+// // // //validation
+// // // console.log(confirm("Are you Sure?"))
+// // // console.log(prompt("enter a value"));
 
-var x= 30;
-console.log(x,typeof(x));
-x="raj"
-console.log(x, typeof x)
-x = true
-console.log(x, typeof x)
-x = 32.3698;
-console.log(x.toFixed(2))
-console.log(x.toPrecision(3))
-x = 23;
-console.log(x.toString(16))
-console.log(Math.abs(-16))
-console.log(Math.sqrt(16))
-console.log(Math.cbrt(27))
-console.log(Math.ceil(-4.1))
-console.log(Math.floor(3.3))
-console.log(Math.round(4.6))
-console.log(Math.pow(2,3), 2**3)
-console.log(Math.max(5, 6))
-//23.6985
-console.log(Math.min(5, 6))
-console.log(Math.round(Math.random() * 10000))
+// // var x= 30;
+// // console.log(x,typeof(x));
+// // x="raj"
+// // console.log(x, typeof x)
+// // x = true
+// // console.log(x, typeof x)
+// // x = 32.3698;
+// // console.log(x.toFixed(2))
+// // console.log(x.toPrecision(3))
+// // x = 23;
+// // console.log(x.toString(16))
+// // console.log(Math.abs(-16))
+// // console.log(Math.sqrt(16))
+// // console.log(Math.cbrt(27))
+// // console.log(Math.ceil(-4.1))
+// // console.log(Math.floor(3.3))
+// // console.log(Math.round(4.6))
+// // console.log(Math.pow(2,3), 2**3)
+// // console.log(Math.max(5, 6))
+// // //23.6985
+// // console.log(Math.min(5, 6))
+// // console.log(Math.round(Math.random() * 10000))
 
 
 
@@ -42,11 +42,119 @@ console.log(Math.round(Math.random() * 10000))
 
 
 
+// var str = "The kalam said \" think and dream big\""
+// console.log(str, typeof (str));
+// // template literal
+// var cost = 200,quanity=5;
+// console.log(`The total cost  is ${cost * quanity}`);
+
+
+// var str = "Js is Very easey"
+// console.log(str.length);
+// console.log(str.indexOf('e'))
+// console.log(str.lastIndexOf('e'));
+// console.log(str.charAt(3))
+// console.log(str.toUpperCase())
+// console.log(str.toLocaleLowerCase())
+// console.log(str.startsWith('Js'))
+// console.log(str.endsWith("y"))
+// var str="prasad"
+// console.log(str.split(''))
+// console.log(["js","is","good"].join(' '))
+
+
+// // reverse the string
+// console.log(str.split('').reverse().join(''))
+
+// console.log(str.replaceAll('a', 'e'));
+// console.log(str.slice(2,5))
+
+// console.log("  shg df ".trim())
+// console.log("this is js demo sentence".split(" ").length)
+// //this is js demo ==> 
+//     // siht si sj omed
+// var arr = "this is js demo".split(' ')
+// let st=""
+// for (var i = 0; i < arr.length; i++)
+// {
+//     st = st + arr[i].split('').reverse().join('') +" "
+//   console.log(st)  
+// }
+
+// /* boolean */
+// console.log(+true, +false)
+
+// // null and undefined , both represents an absence of value but undefiend expects the value in future
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(x);
+var x = 20;
+console.log(x)
+var y = 50;
+function display ()
+{
+    var a = 52;
+    console.log(a)
+}
+display();
 
 
 
