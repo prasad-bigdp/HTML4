6f5c2afc10a717b19de5de7503e347b810874489
prasad-bigdp <bigdp585@gmail.com>
2024-05-26 13:27:07 +0530

    first commit

--- JS/Code/DOM/script.js
+++ JS/Code/DOM/script.js
@@ -3,3 +3,9 @@ function fun ()
    document.getElementById('y').innerHTML = "<b>Hello world</b>"
    document.body.style.backgroundColor="green"
 }
+document.getElementById('btn')
+   .addEventListener('click', x)
+function x ()
+{
+   document.getElementById('modal').classList.toggle('hide')
+}
